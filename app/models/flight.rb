2b3829bededb8a53a2c848ba3f5e3fbf3f1cee24
gonjavi <javiercaliescali@gmail.com>
2020-04-23 17:18:30 -0500

    add content to the flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,14 @@
 class Flight < ApplicationRecord
+  belongs_to :departure_airport, class_name:  'Airport'
+  belongs_to :arrival_airport, class_name: 'Airport'
+  has_many :passengers
+  has_many :bookings
+
+  def duration
+    arriving - leaving
+  end
+
+  def duration=(duration)
+    self.leaving = arriving + duration
+  end
 end
