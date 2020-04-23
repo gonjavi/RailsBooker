a70140126c4a102ab0afe31571cf338d2d11bf48
gonjavi <javiercaliescali@gmail.com>
2020-04-23 17:37:35 -0500

    add controllers

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,33 @@
+class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new(flight_id: params[:flight])
+
+    params[:num_passengers].to_i.times {@booking.passengers.build(
+      flight_id:@booking.flight_id)}
+
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      flash[:info] = 'Booking confirmed'
+      @booking.passengers.each do |p|
+        PassengerMailer.with(passenger: p).thank_you.deliver_later
+      end
+      redirect_to booking_path(@booking)
+    else
+      flash.now[:error] = 'Details incorrect'
+      render 'new'
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:flight_id,
+      passengers_attributes: [:name,:email,:flight_id])
+  end
+end
